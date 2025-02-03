a35fc1cb400f4d3a88fc3d220db2e7a7d11f37ad
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-02-03 17:35:53 -0300

    add with, function

--- script.py
+++ script.py
@@ -1,3 +1,26 @@
 print("Welcome to Python!")
-input("What is your name? ")
+
+# this is a comment 
+
+name = "Placeholder"
+print(name)
+
+name = input("What is your name? ")
+print("Welcome to Python, " + name)
+
+# "a" means to "add"
+
+# "f" means f string which is used to concatenate
+
+# "\n" is used to break the line 
+
+with open("data_base.csv", "a") as databasefile:
+    databasefile.write(f"Welcome to Python {name}.\n")
+    
+age = int(input("How old are you? "))
+
+if age > 21:
+    print("You can join the party!")
+else:
+    print("You can not join the party!")
 
